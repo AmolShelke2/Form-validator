43f2fb8cfa4361156ae14c761ba762a1abbf24ce
Amol shelke <shelkeamol897@gmail.com>
2022-09-28 22:18:26 +0530

    Added show input error message function

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
 
 // show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = "form-control error";
+  const small = formControl.querySelector("small");
+  small.innerText = message;
+}
+
+//
