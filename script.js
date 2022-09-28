2a8c6dda8aa0488491c61be13d2bed73950daca5
Amol shelke <shelkeamol897@gmail.com>
2022-09-28 22:30:15 +0530

    Added checkEmail function

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
 }
+
+// Check email is valid
+function checkEmail(input) {
+  const re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (re.test(input.value.trim())) {
+    showSuccess(input);
+  } else {
+    showError(input, "Email is not valid");
+  }
+}
+
+// Event Handlers
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  checkEmail(email);
+});
