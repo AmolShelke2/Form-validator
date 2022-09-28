45e1c5979d2fcf83c5d7a9196f3bf6f4255235bb
Amol shelke <shelkeamol897@gmail.com>
2022-09-28 22:13:46 +0530

    Selected elements and adding show input error message function

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-// Adding Javascript code.
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("password2");
+
+// show input error message
