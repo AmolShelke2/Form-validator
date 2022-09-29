8b25e5d874c1e0012a14f3fb1a104a3f4448ec29
Amol shelke <shelkeamol897@gmail.com>
2022-09-29 17:44:03 +0530

    Added check field required function

--- script.js
+++ script.js
@@ -30,6 +30,24 @@ function checkEmail(input) {
 }
 
 // check require fields
+function checkRequired(inputArr) {
+  let isRequired = false;
+  inputArr.forEach(function (input) {
+    if (input.value.trim() === "") {
+      showError(input, `${getFieldName(input)} is required`);
+      isRequired = true;
+    } else {
+      showSuccess(input);
+    }
+  });
+
+  return isRequired;
+}
+
+// Get fieldname
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 
 // Event Handlers
 form.addEventListener("submit", function (e) {
